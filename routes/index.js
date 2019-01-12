58ccc4bac9d0c6429484b0c129a049fe511b3c9a
Isaac Peiris <isaacpeiris@icloud.com>
2019-01-12 15:45:17 +1100

    Setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('pages/about');
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('pages/services');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('pages/contact');
+});
+
+router.get('/terms', function(req, res, next) {
+  res.render('pages/terms');
+});
+
+router.get('/privacy', function(req, res, next) {
+  res.render('pages/privacy');
 });
 
 module.exports = router;
