5918311de787278d7a31efe09b5c61480eaaf25d
Isaac Peiris <isaacpeiris@icloud.com>
2019-01-13 17:28:13 +1100

    mobile update about page

---
+++ public/javascripts/index.js
@@ -0,0 +1,25 @@
+
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//       var element = document.getElementById('banner')
+//       if($(this).scrollTop() = 0) { 
+//           $(element.style.transform = "skewY(-4deg)");
+//       } if($(this).scrollTop() = 100) { 
+//         $(element.style.transform = "skewY(0deg)");
+//       }
+//     });
+// });
+
+// function scrollSkew {
+//     var element = document.getElementById("banner");
+//     if (element.scrollTop = 0) {
+//         element.style.transform = "skewY(-4deg";
+//     }
+//     else {
+//         element.style.transform = "skewY(0deg)";
+//     }
+// }
+
+// o.a.createElement("rect",{stroke:"#FADB14",strokeWidth:"1.6",width:"9",height:"9"}),
+// o.a.createElement(u.a,{location:"banner",className:"banner-bg",animation:{playScale:[1,1.5],rotate:0}}),
+// o.a.createElement(l.a,{className:"".concat(t," page"),type:"alpha",delay:150})
