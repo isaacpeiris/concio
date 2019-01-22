c3aacab88e46c3521b94759400281843fc2e5e65
Isaac Peiris <isaacpeiris@icloud.com>
2019-01-22 18:17:20 +1100

    package + node module update

--- app.js
+++ app.js
@@ -9,24 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// Scroll animation
-// var ScrollAnim = require('rc-scroll-anim');
-// var ScrollOverPack = ScrollAnim.OverPack;
-// var React = require('react');
-
-// React.render(<ScrollOverPack>
-//   <QueueAnim key='queueAnim'>
-//     <div key='a'>enter</div>
-//     <div key='b'>enter</div>
-//     <div key='b'>enter</div>
-//   </QueueAnim>
-//   <TweenOne key='tweenOne' vars={{x:100}}>one element</TweenOne>
-//   <Animate key='rc-animate' transitionName="fade" transitionAppear>rc-animate</Animate>
-// </ScrollOverPack>, container);
-
-// var ScrollParallax = ScrollAnim.Parallax;
-// React.render(<ScrollParallax animation={{x:100}}>Parallax</ScrollParallax>,container);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
