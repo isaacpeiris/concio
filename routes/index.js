9f5cfac4c56a96e9e8d7be6237c2f24d81f1b056
Isaac Peiris <isaacpeiris@icloud.com>
2019-02-02 21:57:11 +1100

    Fixed index form submission

--- routes/index.js
+++ routes/index.js
@@ -51,4 +51,73 @@ router.get('/thankyou', function(req, res, next) {
   res.render('pages/thankyou', { title: 'Concio Group | Toolkit' });
 });
 
+// FORM
+
+const axios = require('axios');
+
+//Add header to all axios requests
+axios.defaults.headers.common['Content-type'] = 'application/json';
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.post('/', (req, res) => {
+    const user = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        company: req.body.company,
+        phone: req.body.phone,
+        email: req.body.email,
+    };
+
+    // Send Slack message to team
+    axios.post('https://hooks.slack.com/services/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',
+        {
+            "text": "New contact from the website 🔥",
+            "attachments": [
+                {
+                    "fields": [
+                        {
+                            "title": "First Name",
+                            "value": user.firstname,
+                            "short": false
+                        },
+                        {
+                            "title": "Last Name",
+                            "value": user.lastname,
+                            "short": false
+                        },
+                        {
+                            "title": "Company",
+                            "value": user.company,
+                            "short": false
+                        },
+                        {
+                            "title": "Phone",
+                            "value": user.phone,
+                            "short": false
+                        },
+                        {
+                            "title": "Email",
+                            "value": user.email,
+                            "short": false
+                        }
+                    ]
+                }
+            ]
+        })
+        .then(res => {
+
+        })
+        .catch(err => {
+            throw new Error(err);
+        });
+
+
+    res.redirect('/thankyou');
+});
+
+module.exports = router;
+
 module.exports = router;
