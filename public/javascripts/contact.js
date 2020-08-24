443f65d377b6d2b23f68034ce11365759f786efd
Isaac Peiris <isaacpeiris@icloud.com>
2020-08-24 18:00:35 +1000

    Re-added validation

--- public/javascripts/contact.js
+++ public/javascripts/contact.js
@@ -1,14 +1,38 @@
 $(document).ready(function () {
 
   if (!localStorage.getItem('user')) {
+      // validate form
+      $("#contactForm").validate({
+          errorElement: 'span',
+          errorClass: 'help-inline',
+          rules: {
+              firstname: "required",
+              lastname: "required",
+              company: "required",
+              phone: {
+                  required: false,
+                  number: true
+              },
+              email: {
+                  required: true,
+                  email: true
+              }
+          }
+      });
+
       // On submit
       $("#contactForm").submit(function(event) {
-        const formValues = $('#contactForm').serializeArray().reduce(function(obj, item) {
-            obj[item.name] = item.value;
-            return obj;
-        }, {});
-        // Save users deets to local storage
-        localStorage.setItem('user', JSON.stringify(formValues));
+          const valid = $("#contactForm").valid();
+
+          // If captcha invalid
+          if (valid) {
+              const formValues = $('#contactForm').serializeArray().reduce(function(obj, item) {
+                  obj[item.name] = item.value;
+                  return obj;
+              }, {});
+              // Save users deets to local storage
+              localStorage.setItem('user', JSON.stringify(formValues));
+          }
       });
   } else {
       // Get user object from local storage
